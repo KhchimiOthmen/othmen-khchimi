1562d4b325c4ac407a499f101ab90103070fff30
tasnimbenslama <93452582+tasnimbenslama@users.noreply.github.com>
2022-12-08 03:09:33 +0100

    Add files via upload

---
+++ src/Entity/Formation.php
@@ -0,0 +1,243 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FormationRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=FormationRepository::class)
+ */
+class Formation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+
+     */
+
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message=" this field is required ")
+     */
+
+    private $description;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datedebut;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datefin;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Etudiant::class, inversedBy="formations")
+     */
+    private $etudiants;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="integer")
+
+     */
+    private $nbplaces;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+      */
+    private $niveau;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $detail;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $jaime;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $jaimepas;
+
+
+    public function __construct()
+    {
+        $this->datedebut = new \DateTime();
+        $this->datefin = new \DateTime();
+        $this->etudiants = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->datedebut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $datedebut): self
+    {
+        $this->datedebut = $datedebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->datefin;
+    }
+
+    public function setDateFin(\DateTimeInterface $datefin): self
+    {
+        $this->datefin = $datefin;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Etudiant>
+     */
+    public function getEtudiants(): Collection
+    {
+        return $this->etudiants;
+    }
+
+    public function addEtudiant(Etudiant $etudiant): self
+    {
+        if (!$this->etudiants->contains($etudiant)) {
+            $this->etudiants[] = $etudiant;
+        }
+
+        return $this;
+    }
+
+    public function removeEtudiant(Etudiant $etudiant): self
+    {
+        $this->etudiants->removeElement($etudiant);
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getNbPlaces(): ?int
+    {
+        return $this->nbplaces;
+    }
+
+    public function setNbPlaces(int $nbplaces): self
+    {
+        $this->nbplaces = $nbplaces;
+
+        return $this;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(string $niveau): self
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->detail;
+    }
+
+    public function setDetail(string $detail): self
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+
+    public function getJaime(): ?int
+    {
+        return $this->jaime;
+    }
+
+    public function setJaime(int $jaime): self
+    {
+        $this->jaime = $jaime;
+
+        return $this;
+    }
+
+    public function getJaimepas(): ?int
+    {
+        return $this->jaimepas;
+    }
+
+    public function setJaimepas(?int $jaimepas): self
+    {
+        $this->jaimepas = $jaimepas;
+
+        return $this;
+    }
+
+
+
+
+}
---
+++ src/Form/FormationFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Formation;
+use Doctrine\DBAL\Types\DateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+
+class FormationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('description',TextareaType::class)
+            ->add('datedebut', DateTimeType::class, [
+                'date_widget' => 'single_text'
+            ])
+            ->add('datefin', DateTimeType::class, [
+                'date_widget' => 'single_text'
+            ])
+            ->add('prix')
+            ->add('nbplaces')
+            ->add('niveau')
+            ->add('detail', FileType::class, [
+
+                'mapped' => false,
+
+                'required' => false,
+
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/jpg',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid Image',
+                    ])
+                ],
+            ])
+            ->add('enregistrer',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Formation::class,
+        ]);
+    }
+}
